cc71d9f4924fc0ac58abae4b126e73df15d88fb9
Rama Singh <email-used-in-ramasingh-25>
2026-02-21 12:41:59 +0530

    [Rama] Add : Implemented Feet Measurement Quality

---
+++ QuantityMeasurementApp.Tests/Test1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantityMeasurementApp;
+
+namespace QuantityMeasurementAppTests
+{
+    [TestClass]
+    public class QuantityMeasurementAppTests
+    {
+        [TestMethod]
+        public void TestEquality_SameValue()
+        {
+            var f1 = new QuantityMeasurementApp.Feet(1.0);
+            var f2 = new QuantityMeasurementApp.Feet(1.0);
+
+            Assert.IsTrue(f1.Equals(f2),
+                "1.0 ft should be equal to 1.0 ft");
+        }
+
+        [TestMethod]
+        public void TestEquality_DifferentValue()
+        {
+            var f1 = new QuantityMeasurementApp.Feet(1.0);
+            var f2 = new QuantityMeasurementApp.Feet(2.0);
+
+            Assert.IsFalse(f1.Equals(f2),
+                "1.0 ft should not equal 2.0 ft");
+        }
+
+        [TestMethod]
+        public void TestEquality_NullComparison()
+        {
+            var f1 = new QuantityMeasurementApp.Feet(1.0);
+
+            Assert.IsFalse(f1.Equals(null),
+                "Feet value should not equal null");
+        }
+
+        [TestMethod]
+        public void TestEquality_NonNumericInput()
+        {
+            var f1 = new QuantityMeasurementApp.Feet(1.0);
+
+            Assert.IsFalse(f1.Equals("1.0"),
+                "Feet object should not equal non-numeric input");
+        }
+
+        [TestMethod]
+        public void TestEquality_SameReference()
+        {
+            var f1 = new QuantityMeasurementApp.Feet(1.0);
+
+            Assert.IsTrue(f1.Equals(f1),
+                "Object should be equal to itself (reflexive)");
+        }
+    }
+}
---
+++ QuantityMeasurementApp/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace QuantityMeasurementApp;
+
+public class Class1
+{
+
+  static void Main(string[] args)
+        {
+            QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+            QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+            
+            bool result = f1.Equals(f2);
+
+            Console.WriteLine("Feet Measurement Equality Check");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Input: 1.0 ft and 1.0 ft");
+            Console.WriteLine($"Output: Equal ({result})");
+
+            Console.ReadLine();
+
+            
+
+        }
+}
