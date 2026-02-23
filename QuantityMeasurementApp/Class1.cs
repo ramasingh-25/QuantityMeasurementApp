78a29e5c00633311528c4c616e9ee12838403cff
Rama Singh <email-used-in-ramasingh-25>
2026-02-23 12:01:44 +0530

    [Rama] Add : Implemented Inches Measurement Quality

--- QuantityMeasurementApp/Class1.cs
+++ QuantityMeasurementApp/Class1.cs
@@ -1,37 +1,53 @@
-﻿public class Class1
-    
+﻿using System;
+using QuantityMeasurementApp;
+
+public class Class1
+{
+    public static bool CompareFeet(double value1, double value2)
     {
-        public static bool CompareFeet(double value1, double value2)
-        {
-            QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(value1);
-            QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(value2);
-
-            return f1.Equals(f2);
-        }
-
-        public static bool CompareInches(double value1, double value2)
-        {
-            QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(value1);
-            QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(value2);
-
-            return i1.Equals(i2);
-        }
-        static void Main(string[] args)
-        {
-             bool inchResult =
-                Class1.CompareInches(1.0, 1.0);
-
-            bool feetResult =
-                Class1.CompareFeet(1.0, 1.0);
-
-            Console.WriteLine("Input: 1.0 inch and 1.0 inch");
-            Console.WriteLine($"Output: Equal ({inchResult})");
-
-            Console.WriteLine();
-
-            Console.WriteLine("Input: 1.0 ft and 1.0 ft");
-            Console.WriteLine($"Output: Equal ({feetResult})");
-
-            Console.ReadLine();
-        }
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+        return f1.Equals(f2);
     }
+
+    public static bool CompareInches(double value1, double value2)
+    {
+        Inches i1 = new Inches(value1);
+        Inches i2 = new Inches(value2);
+        return i1.Equals(i2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantity Measurement Equality Check");
+        Console.WriteLine("----------------------------------");
+
+        Console.Write("Enter first feet value: ");
+        double feet1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second feet value: ");
+        double feet2 = Convert.ToDouble(Console.ReadLine());
+
+        bool feetResult = CompareFeet(feet1, feet2);
+
+        Console.WriteLine();
+        Console.WriteLine("Input: " + feet1 + " ft and " + feet2 + " ft");
+        Console.WriteLine("Output: Equal (" + feetResult + ")");
+
+        Console.WriteLine();
+
+        Console.Write("Enter first inches value: ");
+        double inch1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second inches value: ");
+        double inch2 = Convert.ToDouble(Console.ReadLine());
+
+        bool inchResult = CompareInches(inch1, inch2);
+
+        Console.WriteLine();
+        Console.WriteLine("Input: " + inch1 + " inch and " + inch2 + " inch");
+        Console.WriteLine("Output: Equal (" + inchResult + ")");
+
+        Console.ReadLine();
+    }
+}
