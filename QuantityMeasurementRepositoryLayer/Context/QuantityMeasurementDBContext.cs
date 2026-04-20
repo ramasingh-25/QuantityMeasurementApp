2533490f9bcbbcf5740491f01883a6358ae713cb
Rama Singh <email-used-in-ramasingh-25>
2026-04-20 16:33:37 +0530

    [Rama]: Add OnModelCreating to ensure Users table is created

--- QuantityMeasurementRepositoryLayer/Context/QuantityMeasurementDBContext.cs
+++ QuantityMeasurementRepositoryLayer/Context/QuantityMeasurementDBContext.cs
@@ -13,4 +13,26 @@ public class QuantityMeasurementDbContext : DbContext
     public DbSet<QuantityMeasurementEntity> QuantityMeasurements { get; set; }
     public DbSet<UserEntity> Users { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure QuantityMeasurements table
+        modelBuilder.Entity<QuantityMeasurementEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.FirstUnit).IsRequired();
+            entity.Property(e => e.SecondUnit).IsRequired();
+            entity.Property(e => e.Operation).IsRequired();
+            entity.Property(e => e.MeasurementType).IsRequired();
+        });
+
+        // Configure Users table
+        modelBuilder.Entity<UserEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.Password).IsRequired();
+            entity.HasIndex(e => e.Email).IsUnique();
+        });
+    }
 }
